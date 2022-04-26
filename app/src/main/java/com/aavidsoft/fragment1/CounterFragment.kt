d55682b4c0ef5a647e385006cb40be9d136546fb
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-04-26 15:43:30 +0530

    Counter functionality without binding

--- app/src/main/java/com/aavidsoft/fragment1/CounterFragment.kt
+++ app/src/main/java/com/aavidsoft/fragment1/CounterFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class CounterFragment : Fragment() {
+
+    private lateinit var txtCount : TextView
+    private lateinit var btnMinus : Button
+    private lateinit var btnPlus : Button
+    var count = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,11 +21,23 @@ class CounterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        var txtInfo = TextView(context)
-        txtInfo.setTextSize(30F)
-        txtInfo.setText("BitCode Technologies")
+        var view = LayoutInflater.from(context).inflate(R.layout.counter_layout, null)
 
-        return txtInfo
+        txtCount = view.findViewById(R.id.txtCount)
+        btnMinus = view.findViewById(R.id.btnMinus)
+        btnPlus = view.findViewById(R.id.btnPlus)
+        txtCount.setText("$count")
+
+        btnMinus.setOnClickListener {
+            --count
+            txtCount.setText("$count")
+        }
+        btnPlus.setOnClickListener {
+            ++count
+            txtCount.setText("$count")
+        }
+
+        return view
     }
 
 }
