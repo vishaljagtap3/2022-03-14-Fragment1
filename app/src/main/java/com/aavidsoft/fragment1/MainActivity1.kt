6f8c336f4786cb38bd998641767189d0c2b9ca53
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-04-27 15:14:15 +0530

    Adding and removing fragments with MainActivity1

---
+++ app/src/main/java/com/aavidsoft/fragment1/MainActivity1.kt
@@ -0,0 +1,45 @@
+package com.aavidsoft.fragment1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aavidsoft.fragment1.databinding.MainActivity1Binding
+
+class MainActivity1 : AppCompatActivity() {
+
+    private lateinit var binding : MainActivity1Binding
+    private var counterFragments = ArrayList<CounterFragment>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = MainActivity1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAddFragment.setOnClickListener {
+            var fragmentTransaction = supportFragmentManager.beginTransaction()
+
+            var counterFragment = CounterFragment()
+            counterFragments.add(counterFragment)
+
+            //fragmentTransaction.add(counterFragment, CounterFragment::class.java.name)
+            fragmentTransaction.add(R.id.mainContainer, counterFragment, "counterFragment")
+            fragmentTransaction.commit()
+        }
+
+        binding.btnRemoveFragment.setOnClickListener {
+
+            if(counterFragments.size <= 0) {
+                return@setOnClickListener
+            }
+
+            var fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.remove(counterFragments[0])
+            fragmentTransaction.commit()
+
+            counterFragments.removeAt(0)
+        }
+
+
+    }
+
+}
